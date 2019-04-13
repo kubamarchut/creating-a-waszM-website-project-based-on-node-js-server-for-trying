a0c5177393b8d7124e2b9502767b33d3ee29a4c0
Kuba <kuba.marchut@gmail.com>
2019-04-13 22:15:32 +0200

    First ideas

---
+++ Wasz Myśliciel/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require("fs");
+var app = express();
+
+app.set('view engine', 'ejs')
+
+
+
+app.use('/assets', express.static('assets'));
+app.use('/articles', express.static('articles'));
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/a/:article', function(req, res){
+  fs.readFile('/articles/'+req.params.article+'.txt', function (err, data) {
+     if (err) {
+        res.render('blad');
+        console.log("error happend");
+     }
+     else res.render('article', {article: + data.toString()});
+     console.log( '/articles/'+req.params.article+'.txt')
+  });
+});
+
+app.listen(3000);
